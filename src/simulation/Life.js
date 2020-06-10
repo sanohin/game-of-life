6c20d7b9172ec8e9fcba58d0b317b8d1e897a854
Aleksandr Anokhin <sasankhin124@gmail.com>
2020-06-11 00:40:51 +0300

    feat(Life): add life implementation

--- src/simulation/Life.js
+++ src/simulation/Life.js
@@ -6,6 +6,11 @@ export type Row = Array<CellState>;
 
 export type LifeState = Array<Row>;
 
+type Position = {
+  row: number,
+  column: number,
+};
+
 export class Life {
   state: LifeState;
 
@@ -18,6 +23,62 @@ export class Life {
   };
 
   tick = () => {
+    const nextState: LifeState = this.state.map((row, rowIndex) => {
+      return row.map((cell, columnIndex) => {
+        const alive = Life.isAlive(cell);
+        const aliveNeighbors = this._getAliveNeighborsCount({
+          column: columnIndex,
+          row: rowIndex,
+        });
+        return this._getNextCellState(alive, aliveNeighbors);
+      });
+    });
+    this.state = nextState;
     return this.state;
+  };
+
+  _isPositionValid = (position: Position) => {
+    const { column, row } = position;
+    return (
+      Array.isArray(this.state[row]) &&
+      typeof this.state[row][column] === "number"
+    );
+  };
+
+  _getAliveNeighborsCount = (position: Position) => {
+    const { column, row } = position;
+    const positionsToCheck = [
+      // top
+      { column: column - 1, row: row },
+      // top right
+      { column: column - 1, row: row + 1 },
+      // right
+      { column: column, row: row + 1 },
+      // bottom right
+      { column: column + 1, row: row + 1 },
+      // bottom
+      { column: column + 1, row: row },
+      // bottom left
+      { column: column + 1, row: row - 1 },
+      // left
+      { column: column, row: row - 1 },
+      // top left
+      { column: column - 1, row: row - 1 },
+    ];
+    return positionsToCheck
+      .filter(this._isPositionValid)
+      .map(({ column, row }) => {
+        return this.state[row][column];
+      })
+      .filter(Life.isAlive).length;
+  };
+
+  _getNextCellState = (isAlive: boolean, aliveNeighbors: number): CellState => {
+    if (isAlive && [2, 3].includes(aliveNeighbors)) {
+      return 1;
+    } else if (!isAlive && aliveNeighbors === 3) {
+      return 1;
+    }
+    return 0;
   };
 }
